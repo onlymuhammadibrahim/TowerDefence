2dd76225c157df6d8a0dfc578998276babf7cfc0
onlymuhammadibrahim <19-10666@formanite.fccollege.edu.pk>
2020-09-29 09:48:06 +0500

    lec 44 complete

--- Tower_Defence/Assets/Scripts/TowerManager.cs
+++ Tower_Defence/Assets/Scripts/TowerManager.cs
@@ -1,14 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
+
 
 public class TowerManager : Singleton<TowerManager> {
 
     private TowerButton towerButtonPressed;
 
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+            if(hit.collider.tag == "Buildsite")
+            {
+                PlaceTower(hit);
+            }
+        }
+    }
+
+    public void PlaceTower(RaycastHit2D hit)
+    {
+        if(!EventSystem.current.IsPointerOverGameObject() && towerButtonPressed != null)
+        {
+            GameObject newTower = Instantiate(towerButtonPressed.TowerObject());
+            newTower.transform.position = hit.transform.position;
+        }
+    }
+
+
     public void SelectedTower(TowerButton towerSelected)
     {
         towerButtonPressed = towerSelected;
-        Debug.Log("pressed" + towerButtonPressed.gameObject);
     }
 
 }
